db52110220ad560c230bab38f1982345ed0a471e
Ubuntu <ubuntu@ip-172-31-22-152.us-west-2.compute.internal>
2013-07-14 16:31:35 +0000

    attempt to read from index.html

--- web.js
+++ web.js
@@ -1,9 +1,10 @@
 var express = require('express');
 
 var app = express.createServer(express.logger());
+var index = buf.toString(fs.readFileSync(index.html))
 
 app.get('/', function(request, response) {
-  response.send('Hello World!2');
+  response.send(index);
 });
 
 var port = process.env.PORT || 5000;
